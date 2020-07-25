f2aecba2424b7373c38a58c810536531c2a6ea7c
Baytik Tumanbaev <jumka.wows@gmail.com>
2020-07-25 14:57:06 +0600

    Изменил пару предложений

--- frontend/src/Containers/Greeting/Greeting.js
+++ frontend/src/Containers/Greeting/Greeting.js
@@ -15,7 +15,9 @@ class Greeting extends Component {
                         </div>
                     </div>
                     <div className="title">
-                        <p className="title_1">Наша компания производит отличные компьтеры пригодные для игр,учебы,програмирования,для просмотра кино
+                        <p className="title_1">Наша компания продаёт качественные компьютеры по низкой цене, пригодные
+                            для
+                            игр,учебы,програмирования, работы
                             и многое другое.</p>
                         <div className="text-1">
                             <p>1</p>
@@ -24,16 +26,19 @@ class Greeting extends Component {
                         <div className="text-2">
                             <p>2</p>
                         </div>
-                        <p className="title_2">Вы можете заказать компютер на ваше усмотрение, то есть вы можете выбрать любые комплектующие для вашего будущего компьютера!</p>
+                        <p className="title_2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum ducimus
+                            eum fuga laudantium nihil numquam praesentium saepe sunt tenetur? Placeat!</p>
                         <hr className="hr2"/>
                         <div className="text-3">
                             <p>3</p>
                         </div>
-                        <p className="title_3">Есть и готовые варианты компьютеров такие как офисные,офисно-игровые и игровые!</p>
+                        <p className="title_3">Большой выбор компьютеров, такие как
+                            офисные, бюджетные-игровые и
+                            игровые!</p>
                     </div>
                     <div className="lets">
-                        <button onClick={() => this.props.history.push('/login')} className="btn effect04" data-sm-link-text="GO"
-                           target="_blank"><span>LETS</span></button>
+                        <button onClick={() => this.props.history.push('/login')} className="btn effect04"
+                                data-sm-link-text="Список компьютеров"><span>Перейти</span></button>
                     </div>
                 </div>
             </div>
