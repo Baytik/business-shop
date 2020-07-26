51103d8d99ffe5056d9e34a1729f5f5b5b3d4f50
Airas <airastolonov05@gmail.com>
2020-07-26 13:14:32 +0600

    Добавил анимации появление элементов!

--- frontend/src/Containers/Greeting/Greeting.js
+++ frontend/src/Containers/Greeting/Greeting.js
@@ -15,9 +15,7 @@ class Greeting extends Component {
                         </div>
                     </div>
                     <div className="title">
-                        <p className="title_1">Наша компания продаёт качественные компьютеры по низкой цене, пригодные
-                            для
-                            игр,учебы,програмирования, работы
+                        <p className="title_1">Наша компания производит отличные компьтеры пригодные для игр,учебы,програмирования,для просмотра кино
                             и многое другое.</p>
                         <div className="text-1">
                             <p>1</p>
@@ -26,19 +24,19 @@ class Greeting extends Component {
                         <div className="text-2">
                             <p>2</p>
                         </div>
-                        <p className="title_2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum ducimus
+                        <p className="title_2 animate__animated animate__fadeInRight">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum ducimus
                             eum fuga laudantium nihil numquam praesentium saepe sunt tenetur? Placeat!</p>
                         <hr className="hr2"/>
                         <div className="text-3">
                             <p>3</p>
                         </div>
-                        <p className="title_3">Большой выбор компьютеров, такие как
+                        <p className="title_3 animate__animated animate__fadeInLeft">Большой выбор компьютеров, такие как
                             офисные, бюджетные-игровые и
                             игровые!</p>
                     </div>
                     <div className="lets">
-                        <button onClick={() => this.props.history.push('/login')} className="btn effect04"
-                                data-sm-link-text="Список компьютеров"><span>Перейти</span></button>
+                        <button onClick={() => this.props.history.push('/login')} className="btn effect04" data-sm-link-text="GO"
+                           target="_blank"><span>LETS</span></button>
                     </div>
                 </div>
             </div>
