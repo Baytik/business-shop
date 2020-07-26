21a3a9411bff1409d70bc19730ffa8b9883b65c9
Airas <airastolonov05@gmail.com>
2020-07-26 23:02:41 +0600

    Сделал адаптивную верстку для приветственной страницы.

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -11,6 +11,7 @@ class Header extends Component {
     };
 
     render() {
+
         return (
             <header className="head">
                 <div className="logo">
--- frontend/src/Containers/Greeting/Greeting.js
+++ frontend/src/Containers/Greeting/Greeting.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './Greeting.css';
+import './GreetingMedia.css';
 import 'animate.css';
 
 class Greeting extends Component {
@@ -12,30 +13,35 @@ class Greeting extends Component {
                         <img src="https://logotypemaker.com/logo-previews/lr_ckd1co74p0002yhetiq7esf06/logo.png" alt=""/>
                     </div>
                     <div className="about_us">
-                        <div>
                             <p className="textAbout animate__animated animate__fadeInDownBig">О нас</p>
-                        </div>
                     </div>
                     <div className="title">
-                        <p className="title_1 animate__animated animate__fadeInLeft">Наша компания производит отличные компьтеры пригодные для игр,учебы,програмирования,для просмотра кино
-                            и многое другое.</p>
+                        <div className="block">
+                        <p className="title_1 animate__animated animate__fadeInLeft">Наша компания продает качественные компьтеры пригодные для игр,учебы,програмирования,<br/> для работы и многое другое.</p>
                         <div className="text-1 animate__animated animate__rollIn">
                             <p>1</p>
                         </div>
+                        </div>
+
+                        <div className="block">
                         <hr className="hr1"/>
                         <div className="text-2 animate__animated animate__fadeInRight">
                             <p>2</p>
                         </div>
                         <p className="title_2 animate__animated animate__fadeInRight">Вы можете заказать компютер на ваше усмотрение, то есть вы можете выбрать любые комплектующие для вашего будущего компьютера!</p>
+                        </div>
+
+                        <div className="block">
                         <hr className="hr2"/>
                         <div className="text-3 animate__animated animate__rollIn">
                             <p>3</p>
                         </div>
-                        <p className="title_3 animate__animated animate__fadeInLeft">Есть и готовые варианты компьютеров такие как офисные,офисно-игровые и игровые!</p>
+                        <p className="title_3 animate__animated animate__fadeInLeft">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, voluptate!</p>
                     </div>
-                    <div className="lets">
-                        <button onClick={() => this.props.history.push('/login')} className="btn effect04" data-sm-link-text="GO"
-                                target="_blank"><span>LETS</span></button>
+                    </div>
+                    <div className="lets animate__animated animate__fadeInLeftBig">
+                        <button onClick={() => this.props.history.push('/login')} className="btn effect04" data-sm-link-text="список компьютеров"
+                                target="_blank"><span>просмотреть</span></button>
                     </div>
                 </div>
             </div>
